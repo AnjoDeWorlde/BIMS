00d4a24a8eca573a4886cb0ca72ee29b8e42862d
AnjoDeWorlde <anthonyjosh1400@gmail.com>
2024-06-01 11:48:41 +0800

    12 hours of the clock

--- src/admin/homeForm.java
+++ src/admin/homeForm.java
@@ -6,19 +6,19 @@ import javax.swing.plaf.basic.BasicInternalFrameUI;
  *
  * @author DERECHO
  */
-public class homeForm extends javax.swing.JInternalFrame {
+public final class homeForm extends javax.swing.JInternalFrame {
 
     /**
      * Creates new form homeForm
      */
     public homeForm() {
         initComponents();
-        
+                
         this.setBorder(javax.swing.BorderFactory.createEmptyBorder(0,0,0,0));
         BasicInternalFrameUI bi = (BasicInternalFrameUI)this.getUI();
         bi.setNorthPane(null);
     }
-    
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -27,13 +27,10 @@ public class homeForm extends javax.swing.JInternalFrame {
         lblgreetings = new javax.swing.JLabel();
         logo = new javax.swing.JLabel();
         randomproduct = new javax.swing.JPanel();
-        directtolistofproducts = new javax.swing.JPanel();
         lblrandomproduct = new javax.swing.JLabel();
         mosthoursuseduser = new javax.swing.JPanel();
-        directtolistsofuser = new javax.swing.JPanel();
         lbluserpicture = new javax.swing.JLabel();
         mostsoldproduct = new javax.swing.JPanel();
-        directtolistofsales = new javax.swing.JPanel();
         soldproduct = new javax.swing.JLabel();
 
         background.setBackground(new java.awt.Color(255, 255, 255));
@@ -53,39 +50,11 @@ public class homeForm extends javax.swing.JInternalFrame {
         randomproduct.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(46, 49, 146), 5));
         randomproduct.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        directtolistofproducts.setBackground(new java.awt.Color(255, 255, 255));
-        directtolistofproducts.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                directtolistofproductsMouseClicked(evt);
-            }
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                directtolistofproductsMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                directtolistofproductsMouseExited(evt);
-            }
-        });
-
         lblrandomproduct.setBackground(new java.awt.Color(255, 255, 255));
         lblrandomproduct.setForeground(new java.awt.Color(46, 49, 146));
         lblrandomproduct.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lblrandomproduct.setText("randomProduct.png");
-
-        javax.swing.GroupLayout directtolistofproductsLayout = new javax.swing.GroupLayout(directtolistofproducts);
-        directtolistofproducts.setLayout(directtolistofproductsLayout);
-        directtolistofproductsLayout.setHorizontalGroup(
-            directtolistofproductsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(lblrandomproduct, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
-        );
-        directtolistofproductsLayout.setVerticalGroup(
-            directtolistofproductsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(directtolistofproductsLayout.createSequentialGroup()
-                .addGap(72, 72, 72)
-                .addComponent(lblrandomproduct)
-                .addContainerGap(72, Short.MAX_VALUE))
-        );
-
-        randomproduct.add(directtolistofproducts, new org.netbeans.lib.awtextra.AbsoluteConstraints(15, 16, -1, -1));
+        randomproduct.add(lblrandomproduct, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 170, 150));
 
         background.add(randomproduct);
         randomproduct.setBounds(80, 70, 210, 190);
@@ -94,39 +63,11 @@ public class homeForm extends javax.swing.JInternalFrame {
         mosthoursuseduser.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(46, 49, 146), 5));
         mosthoursuseduser.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        directtolistsofuser.setBackground(new java.awt.Color(255, 255, 255));
-        directtolistsofuser.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                directtolistsofuserMouseClicked(evt);
-            }
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                directtolistsofuserMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                directtolistsofuserMouseExited(evt);
-            }
-        });
-
         lbluserpicture.setBackground(new java.awt.Color(255, 255, 255));
         lbluserpicture.setForeground(new java.awt.Color(46, 49, 146));
         lbluserpicture.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lbluserpicture.setText("userpicture.png");
-
-        javax.swing.GroupLayout directtolistsofuserLayout = new javax.swing.GroupLayout(directtolistsofuser);
-        directtolistsofuser.setLayout(directtolistsofuserLayout);
-        directtolistsofuserLayout.setHorizontalGroup(
-            directtolistsofuserLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(lbluserpicture, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
-        );
-        directtolistsofuserLayout.setVerticalGroup(
-            directtolistsofuserLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(directtolistsofuserLayout.createSequentialGroup()
-                .addGap(72, 72, 72)
-                .addComponent(lbluserpicture)
-                .addContainerGap(72, Short.MAX_VALUE))
-        );
-
-        mosthoursuseduser.add(directtolistsofuser, new org.netbeans.lib.awtextra.AbsoluteConstraints(15, 16, -1, -1));
+        mosthoursuseduser.add(lbluserpicture, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 170, 150));
 
         background.add(mosthoursuseduser);
         mosthoursuseduser.setBounds(320, 180, 210, 190);
@@ -135,39 +76,11 @@ public class homeForm extends javax.swing.JInternalFrame {
         mostsoldproduct.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(46, 49, 146), 5));
         mostsoldproduct.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        directtolistofsales.setBackground(new java.awt.Color(255, 255, 255));
-        directtolistofsales.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                directtolistofsalesMouseClicked(evt);
-            }
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                directtolistofsalesMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                directtolistofsalesMouseExited(evt);
-            }
-        });
-
         soldproduct.setBackground(new java.awt.Color(255, 255, 255));
         soldproduct.setForeground(new java.awt.Color(46, 49, 146));
         soldproduct.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         soldproduct.setText("soldProduct.png");
-
-        javax.swing.GroupLayout directtolistofsalesLayout = new javax.swing.GroupLayout(directtolistofsales);
-        directtolistofsales.setLayout(directtolistofsalesLayout);
-        directtolistofsalesLayout.setHorizontalGroup(
-            directtolistofsalesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(soldproduct, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
-        );
-        directtolistofsalesLayout.setVerticalGroup(
-            directtolistofsalesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(directtolistofsalesLayout.createSequentialGroup()
-                .addGap(72, 72, 72)
-                .addComponent(soldproduct)
-                .addContainerGap(72, Short.MAX_VALUE))
-        );
-
-        mostsoldproduct.add(directtolistofsales, new org.netbeans.lib.awtextra.AbsoluteConstraints(15, 16, -1, -1));
+        mostsoldproduct.add(soldproduct, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, 170, 150));
 
         background.add(mostsoldproduct);
         mostsoldproduct.setBounds(20, 270, 210, 190);
@@ -186,47 +99,8 @@ public class homeForm extends javax.swing.JInternalFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void directtolistofproductsMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_directtolistofproductsMouseClicked
-        // TODO add your handling code here:
-    }//GEN-LAST:event_directtolistofproductsMouseClicked
-
-    private void directtolistofproductsMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_directtolistofproductsMouseEntered
-        // TODO add your handling code here:
-    }//GEN-LAST:event_directtolistofproductsMouseEntered
-
-    private void directtolistofproductsMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_directtolistofproductsMouseExited
-        // TODO add your handling code here:
-    }//GEN-LAST:event_directtolistofproductsMouseExited
-
-    private void directtolistsofuserMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_directtolistsofuserMouseClicked
-        // TODO add your handling code here:
-    }//GEN-LAST:event_directtolistsofuserMouseClicked
-
-    private void directtolistsofuserMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_directtolistsofuserMouseEntered
-        // TODO add your handling code here:
-    }//GEN-LAST:event_directtolistsofuserMouseEntered
-
-    private void directtolistsofuserMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_directtolistsofuserMouseExited
-        // TODO add your handling code here:
-    }//GEN-LAST:event_directtolistsofuserMouseExited
-
-    private void directtolistofsalesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_directtolistofsalesMouseClicked
-        // TODO add your handling code here:
-    }//GEN-LAST:event_directtolistofsalesMouseClicked
-
-    private void directtolistofsalesMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_directtolistofsalesMouseEntered
-        // TODO add your handling code here:
-    }//GEN-LAST:event_directtolistofsalesMouseEntered
-
-    private void directtolistofsalesMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_directtolistofsalesMouseExited
-        // TODO add your handling code here:
-    }//GEN-LAST:event_directtolistofsalesMouseExited
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel background;
-    private javax.swing.JPanel directtolistofproducts;
-    private javax.swing.JPanel directtolistofsales;
-    private javax.swing.JPanel directtolistsofuser;
     private javax.swing.JLabel lblgreetings;
     private javax.swing.JLabel lblrandomproduct;
     private javax.swing.JLabel lbluserpicture;
